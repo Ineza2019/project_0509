412f105d8eaddaf22bb1f9cec52e1febb1f0920f
Inna <9592480@gmail.com>
2019-09-05 13:21:36 +0300

    fist commit create

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict'; 
+// обязательная строка для контроля стандарта
+
+
+// var LeftBorderWidght=1;
+// let second=2;
+// const pi = 3.14;
+
+// console.log(LeftBorderWidght);
+// console.log(second);
+
+// let person= {
+// name: "Jonn",
+// age:25,
+// isMarrige: false
+// };
+// console.log(person ['name'])  ;
+
+// let arr = ['plan.png', 'orange.jpeg'];
+// console.log(arr[1]);
+
+// // let anser = confirm ("How do yo do?" );
+// // console.log(anser);
+
+// let anser = prompt ("есть ли вам 18?", "yes" );
+// console.log(anser);
+
+// // console.log(typeof(anser)); тип данных
+
+console.log(typeof(arr)); 
+// тип данных - massiv
+
+// 7 Операторы
+let incr=10, decr=10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+console.log(++incr);
+
+console.log(5/2);
+
+console.log('2'== 2);
+console.log('2'=== 2);
+
+let isChecked=true;
+let isClose=false;
+
+console.log (isChecked && isClose);
+console.log (isChecked || isClose);
+// && оператор и
+// || оператор или 
+// console.log (isChecked || isClose); возвращает истину если хотя бы один истина
+
+// 8 cистема контроля GIT
